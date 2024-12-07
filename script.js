4f9655585674d97fa76522d474d18b73ff321992
caopv <caopv123@gmail.com>
2024-12-07 16:27:00 +0700

    update

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let lastScrollTop = 0
+let isScrollUp = true
 document.addEventListener("scroll", function() {
-    const s = document.querySelector('.a1');
-
     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
     if (currentScroll > lastScrollTop) {
         // Người dùng cuộn xuống
-        s.classList.add('hide');
+        isScrollUp = false
     } else {
         // Người dùng cuộn lên
-        s.classList.remove('hide');
+        isScrollUp = true
     }
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 
 });
+
+setInterval(() => {
+    if(isScrollUp) {
+        const s = document.querySelector('.a1').classList.remove('hide');
+    } else {
+        const s = document.querySelector('.a1').classList.add('hide');
+    }
+}, 200)
