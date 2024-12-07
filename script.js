bb55415fc99d5e74588809f7c5db4bdfd84622e2
caopv <caopv123@gmail.com>
2024-12-07 18:12:42 +0700

    update

--- script.js
+++ script.js
@@ -20,3 +20,68 @@ setInterval(() => {
         const s = document.querySelector('.a1').classList.add('hide');
     }
 }, 200)
+
+function submitForm() {
+    if(window.isSubmit) {
+        showToastErr("Bạn đã phản hồi trước đó. Xin cảm ơn!")
+        remove
+    }
+    var form = document.getElementById('weddingForm');
+    var formData = new FormData(form);
+    if(formData.get("ten").trim().length == 0) {
+        showToastErr("Bạn hãy nhập tên của mình trước khi nhấn gửi")
+        return
+    }
+    if(formData.get("thamGia") == null) {
+        showToastErr("Bạn hãy lựa chọn có tham gia hay không trước khi nhấn gửi")
+        return
+    }
+    window.isSubmit = true
+    send({
+        ten: formData.get("ten"),
+        sdt: formData.get("sdt"),
+        thamGia: formData.get("thamGia")
+    })
+}
+
+function send(data) {
+    var loading = document.getElementById("loading");
+    loading.classList.add("show");
+    fetch('https://formspree.io/f/mdkovojj', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'  // Chỉ định kiểu dữ liệu gửi đi là JSON
+        },
+        body: JSON.stringify(data)  // Chuyển đổi dữ liệu thành chuỗi JSON
+      })
+        .then(response => response.json())  // Nhận phản hồi và chuyển đổi nó thành JSON
+        .then(data => {
+            loading.classList.remove("show");
+            showToast()
+        })
+        .catch(error => {
+            loading.classList.remove("show");
+            console.error('Có lỗi xảy ra:', error);  // Xử lý lỗi
+        });
+}
+
+function showToast() {
+    var toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    // Ẩn thông báo sau 3 giây
+    setTimeout(function() {
+      toast.classList.remove("show");
+    }, 3000);
+}
+
+function showToastErr(msg) {
+    var toast = document.getElementById("toastErr");
+    toast.innerHTML = msg
+    toast.classList.add("show");
+
+    // Ẩn thông báo sau 3 giây
+    setTimeout(function() {
+      toast.classList.remove("show");
+    }, 3000);
+}
